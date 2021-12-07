6938bd66c62c55368e9d1126a095063cb22c54b8
Arib <aribchowdhury03@gmail.com>
2021-12-07 01:16:13 -0500

    day7

---
+++ day7/code.py
@@ -0,0 +1,20 @@
+import numpy as np #np arrays are just nice in general
+import re #re.split(), re.match, re.findall, Regex Guide at https://www.debuggex.com/cheatsheet/regex/python
+import collections #defaultdicts, maybe counters
+import functools #use @functools.lru_cache(None) above a function to keep track of all inputs and speed it up
+import string #ascii_uppercase, ascii_lowercase, etc..
+import itertools #itertools.product(range(a),range(b),[1,2,3]) allows for nested for loops in one line essentially
+from copy import deepcopy #deepcopy() let's use copy a list without reference areas
+#Sets are POG {}, A|B finds union of sets, A&B finds intersection, A-B finds difference, A^B is (A|B)-(A&B)
+#Python has complex numbers of the form x+yj (1+1j) or complex(x,y) (complex(1,1))
+
+def run(filename):
+    print("Running",filename+"...")
+
+    lines = list(map(int,open(filename).read().split(',')))
+    print(min(sum(abs(i-j) for i in lines) for j in range(max(lines))))
+    print(min(sum(abs(i-j)*(abs(i-j)+1)//2 for i in lines) for j in range(max(lines))))
+
+run("example.txt")
+print()
+run("day7.txt")
